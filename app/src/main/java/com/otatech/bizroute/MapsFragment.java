71d781a947e656604d06de9e331f3a0779175323
jnemesis <mpadair00@gmail.com>
2015-12-28 13:55:21 -0500

    3

--- app/src/main/java/com/otatech/bizroute/MapsFragment.java
+++ app/src/main/java/com/otatech/bizroute/MapsFragment.java
@@ -1,18 +1,29 @@
 package com.otatech.bizroute;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+
+import static android.Manifest.permission.*;
+import static android.support.v4.app.ActivityCompat.*;
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
 
 /**
  * Created by raunie.adair on 12/23/2015.
@@ -26,11 +37,11 @@ public class MapsFragment extends SupportMapFragment implements GoogleApiClient.
 
     private GoogleApiClient mGoogleApiClient;
     private Location mCurrentLocation;
-    private final int[] MAP_TYPES = { GoogleMap.MAP_TYPE_SATELLITE,
-                                        GoogleMap.MAP_TYPE_NORMAL,
-                                        GoogleMap.MAP_TYPE_HYBRID,
-                                        GoogleMap.MAP_TYPE_TERRAIN,
-                                        GoogleMap.MAP_TYPE_NONE };
+    private final int[] MAP_TYPES = {GoogleMap.MAP_TYPE_SATELLITE,
+            GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_NONE};
 
     private int curMapTypeIndex = 0;
 
@@ -39,7 +50,7 @@ public class MapsFragment extends SupportMapFragment implements GoogleApiClient.
         super.onViewCreated(view, savedInstanceState);
 
         setHasOptionsMenu(true);
-        mGoogleApiClient = new GoogleApiClient.Builder( getActivity() )
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
@@ -56,8 +67,41 @@ public class MapsFragment extends SupportMapFragment implements GoogleApiClient.
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    public void onStop() {
+        super.onStart();
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        initCamera(mCurrentLocation);
+    }
+
+    private void initCamera(Location location) {
+        CameraPosition position = CameraPosition.builder()
+                .target(new LatLng(location.getLatitude(),
+                        location.getLongitude()))
+                .zoom(16f)
+                .bearing(0.0f)
+                .tilt(0.0f)
+                .build();
+        getMap().animateCamera(CameraUpdateFactory
+        .newCameraPosition(position), null);
+
+        getMap().setMapType(MAP_TYPES[curMapTypeIndex]);
+        getMap().setTrafficEnabled(true);
+        getMap().setMyLocationEnabled(true);
+        getMap().getUiSettings().setZoomControlsEnabled(true);
     }
 
     @Override
